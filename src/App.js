a26ef01cacd0d6330774b27ab7d88a9be6b9a818
Djavan Munroe <djavanm24@gmail.com>
2021-05-13 23:52:34 -0600

    update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase";
+import "firebase/firestore";
+
+import CanPicker from "./Components/CanPicker";
+import CannedMessagesContainer from "./Components/CannedMessagesContainer";
+import CannedMessageSender from "./Components/CannedMessageSender";
+
+import "./App.css";
+
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA4n5bWHDz4b4501hjuhkCzW_7sm29fARE",
+  authDomain: "two-cans.firebaseapp.com",
+  projectId: "two-cans",
+  storageBucket: "two-cans.appspot.com",
+  messagingSenderId: "337405766237",
+  appId: "1:337405766237:web:10d0b6c0b8ba7c659e3bb4",
+});
+
+const firestore = firebase.firestore();
 
 function App() {
+  const [userId, setUserId] = React.useState(null);
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const addMessage = async (text) => {
+    await messagesRef.add({
+      userId,
+      text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
+
+  const selectCan = (id) => {
+    setUserId(id);
+  };
+
+  const handleLogOut = () => {
+    setUserId(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Canned Messages </h1>
+        {userId === 1234 && <button onClick={handleLogOut}>Logout 🥫</button>}
+        {userId === 4321 && <button onClick={handleLogOut}>Logout 🗑</button>}
       </header>
+      {!userId && <CanPicker selectCan={selectCan} />}
+      {userId && (
+        <>
+          <CannedMessagesContainer messages={messages} />
+          <CannedMessageSender addMessage={addMessage} />
+        </>
+      )}
     </div>
   );
 }
